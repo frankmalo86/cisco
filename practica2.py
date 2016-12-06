9a2bd64884ae9eb89d2b6d1e1bf7f6ef0d80a627
frankmalo86 <frankmalo86@gmail.com>
2016-12-05 21:40:24 -0500

    practicas

---
+++ practica2.py
@@ -0,0 +1,47 @@
+print ("---------- TIPOS DE DATOS ------------")
+
+#tipos de datos
+
+# print(type (10))
+# print(type (5.5))
+# print(type (1+5j))
+
+
+print(type (10))
+print(type (5.5))
+print(type(1+5j))
+
+
+print (2 +4)
+print (8**3)
+print ((1+5j) + (1+8j))
+
+
+print ("esto es una cadena")
+print ('esto es una cadena con comillas simples')
+print ("""
+linea1
+linea2
+linea3
+""")
+
+print(type("esto es una cadena"))
+
+print ("Frank " + "Malo")
+print ("cadena 1 " * 10)
+
+print(type(True))
+print(type(False))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practica3.py
@@ -0,0 +1,51 @@
+IVA = 12
+
+a = 5
+print (a)
+print (type(a))
+a = "cinco"
+print (a)
+print (type(a))
+
+
+a = 10
+#a = a + 9
+a+=9
+print (a)
+
+
+
+print (3 & 2)
+print (3 << 2)
+
+
+nombre1 = "frank"
+nombre2 = "Frank"
+print (nombre1 == nombre2)
+
+
+anio_nacimiento = 1986
+print ("Mayor de edad")
+print ((2016 - anio_nacimiento) >= 18)
+
+edad = 60
+print ("Adolescente ??  12 - 18   ")
+print ( edad >= 12 and edad <= 18 )
+print ( 12 <= edad <= 18 )
+
+print ("edad no laboral ??")
+print ( 1 <= edad <= 18 or  80 <= edad <= 120 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
